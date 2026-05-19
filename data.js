0c88416805b940ae5b2c16bbd79d1ec22c6cdcec
jamitinoku <densan@mitinoku.or.jp>
2026-05-19 14:49:27 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+const cmsData = {
+  siteInfo: {
+    title: "簡易ツール",
+    subtitle: "便利なツールを"
+  },
+  
+  news: [
+    {
+      id: 3,
+      date: "",
+      category: "",
+      title: "",
+      content: "",
+      url: "" // ← リンクがない場合は空欄にするだけ
+    },
+    {
+      id: 2,
+      date: "",
+      category: "",
+      title: "",
+      content: "",
+      url: "" // ← リンクがない場合は空欄にするだけ
+    },
+    {
+      id: 1,
+      date: "2026年05月19日",
+      category: "総務",
+      title: "簡易文字起こしツール",
+      content: "Googleの文字認識サーバ利用の文字起こしツール。Chrome推奨",
+      url: "https://jamitinoku.github.io/transcribe.html" // ← リンク先を指定
+    }
+  ]
+};
