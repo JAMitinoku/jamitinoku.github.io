33d2275e9ba77504067b7a0e2c06ade7b098aa23
jamitinoku <densan@mitinoku.or.jp>
2026-05-22 06:55:25 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -6,20 +6,12 @@ const cmsData = {
   
   news: [
     {
-      id: 4,
+      id: 3,
       date: "2026年05月20日",
       category: "管理",
       title: "録音ファイルから文字起こし",
       content: "【イヤホン利用推奨】音声を再生しながら、文字起こし（編集可）",
       url: "https://jamitinoku.github.io/transcribe-file.html" // ← リンクがない場合は空欄にするだけ
-    },
-    {
-      id: 3,
-      date: "2026年05月19日",
-      category: "",
-      title: "NDLOCR-Lite Web",
-      content: "国立国会図書館が開発した、AI利用の日本語OCR",
-      url: "https://ndlocr-liteweb.netlify.app/" // ← リンクがない場合は空欄にするだけ
     },
     {
       id: 2,
