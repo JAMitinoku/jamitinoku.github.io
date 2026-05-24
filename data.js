628d53deb9b5e15fb3e27ce13e88216c274c80a9
jamitinoku <densan@mitinoku.or.jp>
2026-05-24 19:06:46 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -9,25 +9,25 @@ const cmsData = {
       id: 3,
       date: "2026年05月20日",
       category: "管理",
-      title: "録音ファイルから文字起こし",
-      content: "【イヤホン利用推奨】音声を再生しながら、文字起こし（編集可）",
-      url: "https://jamitinoku.github.io/transcribe-file.html" // ← リンクがない場合は空欄にするだけ
+      title: "文字起こし",
+      content: "「リアルタイム」と「音声ファイル」両対応の文字起こし（編集可）",
+      url: "https://jamitinoku.github.io/transcribex.html" // ← リンクがない場合は空欄にするだけ
     },
     {
       id: 2,
       date: "2026年05月19日",
       category: "",
-      title: "簡易文字起こしツール（スマホ）",
-      content: "Googleの文字認識サーバ利用の文字起こしツール。Chrome推奨",
-      url: "https://jamitinoku.github.io/transcribes.html" // ← リンク先を指定
-    },
-    {
-      id: 1,
-      date: "2026年05月19日",
-      category: "",
       title: "簡易文字起こしツール（PC）",
       content: "Googleの文字認識サーバ利用の文字起こしツール。Chrome推奨",
       url: "https://jamitinoku.github.io/transcribex.html" // ← リンク先を指定
+    },
+    {
+      id: 1,
+      date: "",
+      category: "",
+      title: "",
+      content: "",
+      url: "" // ← リンク先を指定
     }
   ]
 };
