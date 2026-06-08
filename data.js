61a70fef9d9ce8296f74a43fd8700658bdfa45de
jamitinoku <densan@mitinoku.or.jp>
2026-06-08 23:39:04 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -9,25 +9,25 @@ const cmsData = {
       id: 3,
       date: "2026年05月25日",
       category: "管理",
-      title: "文字起こし「編集可」",
+      title: "文字起こし「Edge」",
       content: "",
-      url: "https://jamitinoku.github.io/transcribe-e5.html" // ← リンクがない場合は空欄にするだけ
+      url: "https://jamitinoku.github.io/transcribe.html" // ← リンクがない場合は空欄にするだけ
     },
     {
       id: 2,
       date: "2026年05月25日",
       category: "管理",
-      title: "文字起こし「Edge推奨」",
+      title: "文字起こし（メディア再生）「Edge推奨」",
       content: "",
       url: "https://jamitinoku.github.io/transcribe-m.html" // ← リンクがない場合は空欄にするだけ
     },
     {
       id: 1,
-      date: "2026年05月25日",
-      category: "管理",
-      title: "文字起こし（編集対応）「Edge推奨」",
+      date: "",
+      category: "",
+      title: "",
       content: "",
-      url: "https://jamitinoku.github.io/transcribe-e.html" // ← リンクがない場合は空欄にするだけ
+      url: "" // ← リンク先を指定
     }
   ]
 };
